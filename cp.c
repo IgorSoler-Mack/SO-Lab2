30fce9524d1b63a1de03c370668419f6c7643d97
IgorSoler-Mack <99455511+IgorSoler-Mack@users.noreply.github.com>
2022-02-24 19:14:11 -0300

    Marcações de Syscall

--- cp.c
+++ cp.c
@@ -6,25 +6,25 @@ int main (int numargs, char *arg[]) {
    FILE *arquivoOrigem;
    FILE *arquivoDestino;
    
-   arquivoOrigem = fopen (arg[1], "r"); <-- System Call  (Abre um aqruivo)
+   arquivoOrigem = fopen (arg[1], "r"); // <-- System Call  (Abre um aqruivo)
    
    if (arquivoOrigem == NULL) {
-      printf ("\nArquivo para ser copiado nao encontrado!\n");
+      printf ("\nArquivo para ser copiado nao encontrado!\n"); // <-- Possível System Call (Imprime uma mensagem no terminal, existem casos em que printf é implementado com a syscall write)
       exit (EXIT_FAILURE);
    }
    //Cria um arquivo caso não exista nenhum com o nome
-   arquivoDestino = fopen(arg[2], "w"); <-- System Call  (Abre um aqruivo)
+   arquivoDestino = fopen(arg[2], "w"); // <-- System Call  (Abre um aqruivo)
    if (arquivoDestino == NULL) {
-      printf("\nNao foi possivel criar/abrir o arquivo de destino!\n");
-      fclose(arquivoOrigem); <-- System Call  (Fecha o arquivo previamente aberto)
+      printf("\nNao foi possivel criar/abrir o arquivo de destino!\n"); // <-- Possível System Call (Imprime uma mensagem no terminal, existem casos em que printf é implementado com a syscall write)
+      fclose(arquivoOrigem);    // <-- System Call  (Fecha o arquivo previamente aberto)
       exit (EXIT_FAILURE);
    }
 
    int c;
-   c = fgetc(arquivoOrigem);
+   c = fgetc(arquivoOrigem);    // <-- System Call (Lê um char de um arquivo)
    while (c != EOF){
-      fputc(c,arquivoDestino); 
-      c = fgetc(arquivoOrigem);
+      fputc(c,arquivoDestino);  // <-- System Call (Imprime uma mensagem no terminal, existem casos em que printf é implementado com a syscall write)
+      c = fgetc(arquivoOrigem); // <-- System Call (Lê um char de um arquivo)
    }
 
    fclose(arquivoOrigem);  <-- System Call  (Fecha o arquivo previamente aberto)
@@ -32,4 +32,4 @@ int main (int numargs, char *arg[]) {
    return EXIT_SUCCESS;
 }
 
-// Total de System Calls 6
+// Total de System Calls 10
