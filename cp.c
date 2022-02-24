f35bbe3afbf522517f4704905ece9ceb106c20bc
RafaelFerreira4 <99454864+RafaelFerreira4@users.noreply.github.com>
2022-02-24 20:18:31 -0300

    Correção de escrita

--- cp.c
+++ cp.c
@@ -6,14 +6,14 @@ int main (int numargs, char *arg[]) {
    FILE *arquivoOrigem;
    FILE *arquivoDestino;
    
-   arquivoOrigem = fopen (arg[1], "r"); // <-- System Call  (Abre um aqruivo)
+   arquivoOrigem = fopen (arg[1], "r"); // <-- System Call  (Abre um arquivo)
    
    if (arquivoOrigem == NULL) {
       printf ("\nArquivo para ser copiado nao encontrado!\n"); // <-- Possível System Call (Imprime uma mensagem no terminal, existem casos em que printf é implementado com a syscall write)
       exit (EXIT_FAILURE);
    }
    //Cria um arquivo caso não exista nenhum com o nome
-   arquivoDestino = fopen(arg[2], "w"); // <-- System Call  (Abre um aqruivo)
+   arquivoDestino = fopen(arg[2], "w"); // <-- System Call  (Abre um arquivo)
    if (arquivoDestino == NULL) {
       printf("\nNao foi possivel criar/abrir o arquivo de destino!\n"); // <-- Possível System Call (Imprime uma mensagem no terminal, existem casos em que printf é implementado com a syscall write)
       fclose(arquivoOrigem);    // <-- System Call  (Fecha o arquivo previamente aberto)
@@ -23,7 +23,7 @@ int main (int numargs, char *arg[]) {
    int c;
    c = fgetc(arquivoOrigem);    // <-- System Call (Lê um char de um arquivo)
    while (c != EOF){
-      fputc(c,arquivoDestino);  // <-- System Call (Imprime uma mensagem no terminal, existem casos em que printf é implementado com a syscall write)
+      fputc(c,arquivoDestino);  // <-- System Call (Escreve o caractere c no arquivo de destino)
       c = fgetc(arquivoOrigem); // <-- System Call (Lê um char de um arquivo)
    }
 
