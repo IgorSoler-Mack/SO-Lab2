098500692bde92aaac45cc1f00b2eac4b2c60650
IgorSoler-Mack <99455511+IgorSoler-Mack@users.noreply.github.com>
2022-02-24 20:21:44 -0300

    Adição de Comentarios

--- cp.c
+++ cp.c
@@ -10,14 +10,14 @@ int main (int numargs, char *arg[]) {
    
    if (arquivoOrigem == NULL) {
       printf ("\nArquivo para ser copiado nao encontrado!\n"); // <-- Possível System Call (Imprime uma mensagem no terminal, existem casos em que printf é implementado com a syscall write)
-      exit (EXIT_FAILURE);
+      exit (EXIT_FAILURE);   // <-- System Call  (Finaliza o Processo)
    }
    //Cria um arquivo caso não exista nenhum com o nome
    arquivoDestino = fopen(arg[2], "w"); // <-- System Call  (Abre um arquivo)
    if (arquivoDestino == NULL) {
       printf("\nNao foi possivel criar/abrir o arquivo de destino!\n"); // <-- Possível System Call (Imprime uma mensagem no terminal, existem casos em que printf é implementado com a syscall write)
       fclose(arquivoOrigem);    // <-- System Call  (Fecha o arquivo previamente aberto)
-      exit (EXIT_FAILURE);
+      exit (EXIT_FAILURE);      // <-- System Call  (Finaliza o Processo)
    }
 
    int c;
