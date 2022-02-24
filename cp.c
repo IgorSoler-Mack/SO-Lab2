ba2af24ab8bef7b23cce105cc25193741e245743
IgorSoler-Mack <99455511+IgorSoler-Mack@users.noreply.github.com>
2022-02-24 19:07:40 -0300

    Marcação de System Calls

--- cp.c
+++ cp.c
@@ -6,28 +6,30 @@ int main (int numargs, char *arg[]) {
    FILE *arquivoOrigem;
    FILE *arquivoDestino;
    
-   arquivoOrigem = fopen (arg[1], "r");
+   arquivoOrigem = fopen (arg[1], "r"); <-- System Call  (Abre um aqruivo)
    
    if (arquivoOrigem == NULL) {
       printf ("\nArquivo para ser copiado nao encontrado!\n");
       exit (EXIT_FAILURE);
    }
    //Cria um arquivo caso não exista nenhum com o nome
-   arquivoDestino = fopen(arg[2], "w");
+   arquivoDestino = fopen(arg[2], "w"); <-- System Call  (Abre um aqruivo)
    if (arquivoDestino == NULL) {
       printf("\nNao foi possivel criar/abrir o arquivo de destino!\n");
-      fclose(arquivoOrigem);
+      fclose(arquivoOrigem); <-- System Call  (Fecha o arquivo previamente aberto)
       exit (EXIT_FAILURE);
    }
 
    int c;
    c = fgetc(arquivoOrigem);
    while (c != EOF){
-      fputc(c,arquivoDestino);
+      fputc(c,arquivoDestino); 
       c = fgetc(arquivoOrigem);
    }
 
-   fclose(arquivoOrigem);
-   fclose(arquivoDestino);
+   fclose(arquivoOrigem);  <-- System Call  (Fecha o arquivo previamente aberto)
+   fclose(arquivoDestino); <-- System Call  (Fecha o arquivo previamente aberto)
    return EXIT_SUCCESS;
 }
+
+// Total de System Calls 6
